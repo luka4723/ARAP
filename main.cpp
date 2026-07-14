27c6076177c6b304f1d3304225ca5815a662c80d
luka <lmilic4723rn@raf.rs>
2026-07-14 17:25:55 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <igl/readOFF.h>
+#include <igl/opengl/glfw/Viewer.h>
+
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+
+int main(int argc, char *argv[])
+{
+  // Load a mesh in OFF format
+  igl::readOFF("C:\\faks\\eth\\ARAP\\ARAP\\res\\armadillo_1k.off", V, F);
+
+  // Plot the mesh
+  igl::opengl::glfw::Viewer viewer;
+  
+  viewer.data().set_mesh(V, F);
+  viewer.launch();
+}
